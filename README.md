d29c2458ea703d0dd129f529c33c3ac5d116bbaf
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-10-19 16:52:12 +0200

    add comments to useStore and create minimal README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Next.js starter
+
+Just another personal starter repo.
+
+Includes
+
+- Prettier
+- ESLint setup with Standard
+- TypeScript support
+- zustand
+- react-query
--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,6 +1,20 @@
 import create from 'zustand';
 
 // https://github.com/pmndrs/zustand
+
+// Initialisation:
+
+// const useStore = create((set) => ({
+//   counter: 0,
+//   increaseCounter: () => set(({ counter }) => ({ counter: counter + 1 })),
+//   resetCounter: () => set({ counter: 0 }),
+// }));
+
 const useStore = create(() => ({}));
 
 export { useStore };
+
+// Usage:
+
+// const counter = useStore(({ counter }) => counter);
+// const increateCounter = useStore(({ increaseCounter }) => increaseCounter);
