ec69b4c14612da802f5c241d771a5e1af3ff3489
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2020-10-21 09:29:00 +0200

    reset some global CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import type { AppProps } from 'next/app';
+
+import { createGlobalStyle } from 'styled-components';
 import { Reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`;
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
     <Reset />
+    <GlobalStyle />
     <Component {...pageProps} />
   </>
 );
